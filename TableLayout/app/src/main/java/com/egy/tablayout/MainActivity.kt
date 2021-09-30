d7b872ee6a18a94cc9d8e8d375128707a359c8c6
egy <ba19d100@gmail.com>
2021-09-30 00:04:10 -0500

    Added

--- TableLayout/app/src/main/java/com/egy/tablayout/MainActivity.kt
+++ TableLayout/app/src/main/java/com/egy/tablayout/MainActivity.kt
@@ -1,28 +1,52 @@
 package com.egy.tablayout
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TableRow
+import android.widget.*
+import androidx.core.view.setPadding
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
+        val add = findViewById<Button>(R.id.add)
+        val table = findViewById<TableLayout>(R.id.table)
+        val txt1 = findViewById<TextView>(R.id.column1)
+        val txt2 = findViewById<TextView>(R.id.column2)
+        add.setOnClickListener {
 
-    open fun add(Version: String, Name: String){}
+            val tableRow = TableRow(applicationContext)
+            val layoutParams = TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT)
+            tableRow.layoutParams = layoutParams
+
+            val columnOne = TextView(this)
+            columnOne.text = txt1.text
+            columnOne.setPadding(10)
+            columnOne.setBackgroundColor(Color.parseColor("#f368e0"))
+
+            val columnTwo = TextView(this)
+            columnTwo.text = txt2.text
+            columnTwo.setPadding(10)
+            columnTwo.setBackgroundColor(Color.parseColor("#f368e0"))
+
+            tableRow.addView(columnOne)
+            tableRow.addView(columnTwo)
+            tableRow.setPadding(0, 0, 0, 10)
+
+            table.addView(tableRow)
+
+            Toast.makeText(applicationContext, "Success", Toast.LENGTH_LONG).show()
+        }
+    }
 
     fun button(view: android.view.View) {
         // Create a new table row.
         val tableRow = TableRow(getApplicationContext())
         // Set new table row layout parameters.
         val layoutParams = TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT)
-        tableRow.setLayoutParams(layoutParams)}
-// add values into row by calling addView()
-//
-//        tableRow.addView(value,column numbers)
-//// Finally add the created row row into layout
-//        tableLayoutid.addView(tableRow) // id from Layout_file
-//    }
+        tableRow.setLayoutParams(layoutParams)
+    }
 }
