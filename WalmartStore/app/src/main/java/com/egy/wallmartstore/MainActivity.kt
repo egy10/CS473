efa907a2c604f21f97f1e0e750473c8869898e55
egy <ba19d100@gmail.com>
2021-09-30 23:57:33 -0500

    Login

--- WalmartStore/app/src/main/java/com/egy/wallmartstore/MainActivity.kt
+++ WalmartStore/app/src/main/java/com/egy/wallmartstore/MainActivity.kt
@@ -6,17 +6,39 @@ import android.os.Bundle
 import android.widget.EditText
 import android.widget.Toast
 
-class  MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        users.add(UserInfo("egy@email.com", "hello"))
+        users.add(UserInfo("bold@email.com", "yes"))
+        users.add(UserInfo("don@email.com", "good"))
+        users.add(UserInfo("batu@email.com", "bold"))
+        users.add(UserInfo("renuka@email.com", "hi"))
+
     }
 
+    var users: MutableList<UserInfo> = mutableListOf()
+
     fun signInImpilicit(view: android.view.View) {
-        val intent = Intent(this, SecondActivity::class.java)
 
         val username = findViewById<EditText>(R.id.etusername)
         val password = findViewById<EditText>(R.id.etpassword)
+
+        var found = false
+        for (user in users) {
+            if (user.username.equals(username.text.toString()) && user.userpassword.equals(password.text.toString()))
+                found = true
+        }
+
+        if (!found) {
+            Toast.makeText(this, "Invalid user info", Toast.LENGTH_LONG).show()
+            return
+        }
+
+
+        val intent = Intent(this, SecondActivity::class.java)
 
         val userInfo = UserInfo(username.text.toString(), password.text.toString())
         intent.putExtra("userinfo", userInfo)
@@ -27,7 +49,7 @@ class  MainActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
-        Toast.makeText(this,"Sain uu",Toast.LENGTH_LONG).show()
+        Toast.makeText(this, "Sain uu", Toast.LENGTH_LONG).show()
     }
 
 }
