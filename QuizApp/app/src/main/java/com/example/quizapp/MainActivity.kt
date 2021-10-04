8a6036329cf71cdf656c2e67313279073228dfbb
egy <ba19d100@gmail.com>
2021-10-03 21:58:06 -0500

    Lab6 Problem 1

--- QuizApp/app/src/main/java/com/example/quizapp/MainActivity.kt
+++ QuizApp/app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.quizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.RadioButton
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main);
+
+//        val submitButton= AlertDialog.Builder(this)
+//            .setTitle("HI")
+//            .setMessage("Hi enkhee")
+//            .setIcon(R.drawable.ic_launcher_background)
+//            .setPositiveButton("Yes"){
+//                dialogInterface, i ->
+//                Toast.makeText(this, "Za positive", Toast.LENGTH_LONG).show()
+//            }
+//            .setNegativeButton("No"){
+//                    dialogInterface, i ->
+//                Toast.makeText(this, "Za neagtave", Toast.LENGTH_LONG).show()
+//            }.create()
+//
     }
 }
