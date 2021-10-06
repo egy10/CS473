35d58ffb11681a961ba4d187b702e54208840e10
egy <ba19d100@gmail.com>
2021-10-05 20:47:02 -0500

    quizApp calculate point and show dialogAlert

--- QuizApp/app/src/main/java/com/example/quizapp/MainActivity.kt
+++ QuizApp/app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -1,28 +1,53 @@
 package com.example.quizapp
 
-import android.annotation.SuppressLint
-import android.content.DialogInterface
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.*
+import android.widget.CheckBox
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 
-class Quiz : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity() {
+    var point = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        supportActionBar?.title = "Quiz App"
+    }
 
-        int points = 0;
+    fun checkQuiz(view: View) {
+        point = 0
+        val radioOne = findViewById<RadioButton>(R.id.radio_one)
+        if( radioOne.isChecked()){
+            point += 50
+        }
 
+        val checkTwo = findViewById<CheckBox>(R.id.check_two)
+        if(checkTwo.isChecked){
+            point += 25
+        }
 
+        val checkFour = findViewById<CheckBox>(R.id.check_four)
+        if(checkFour.isChecked){
+            point += 25
+        }
 
+        val alertDialog = AlertDialog.Builder(this@MainActivity).create()
+        alertDialog.setTitle("Result")
+        alertDialog.setMessage("Total score: " +point)
+        alertDialog.setButton(
+            AlertDialog.BUTTON_POSITIVE, "OK"
+        ) { dialog, which -> dialog.dismiss() }
+        alertDialog.show()
+    }
 
+    fun resetQuiz(view: View) {
+        findViewById<RadioGroup>(R.id.rg).clearCheck()
+        findViewById<CheckBox>(R.id.check_one).setChecked(false)
+        findViewById<CheckBox>(R.id.check_two).setChecked(false)
+        findViewById<CheckBox>(R.id.check_three).setChecked(false)
+        findViewById<CheckBox>(R.id.check_four).setChecked(false)
     }
 }
-
-
-
-
-
